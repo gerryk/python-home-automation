ba5c0db7b2f65fcc7e8ab94abf7308c0f0c50dec
Gerry Kavanagh <gerry.kavanagh@ericsson.com>
2013-07-25 17:11:55 +0100

    Start building methods

--- locations.py
+++ locations.py
@@ -10,10 +10,47 @@ class Location():
      
 
 class Room(Location):
-    pass
+    def __init__(self, n):
+        self.name = n
+        self.devices = []
+        
+    def add_device(self, d):
+        self.devices.append(d)
 
+    def get_devices(self):
+        return self.devices
+    
+    def all_on(self):
+        for d in self.devices:
+            if d.type == 'switch':
+                d.on()
+                
+    def all_off(self):
+        for d in self.devices:
+            if d.type == 'switch':
+                d.off()
+        
+        
 class Floor(Location):
-    pass
+    def __init__(self, n):
+        self.name = n
+        self.rooms = []
+        
+    def add_room(self, r):
+        self.rooms.append(r)
+        
+    def get_rooms(self):
+        return self.rooms
+    
+    def all_on(self):
+        for r in self.rooms:
+            r.all_on()
+            
+    def all_off(self):
+        for r in self.rooms:
+            r.all_off()
+            
+
 
 
         
